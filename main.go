b8318af63aa86dd37b9ce094d6bf7ef6dbd0367f
Kayo Ronald <kayoronald4321@gmail.com>
2023-07-11 13:21:13 +0000

    feat: update main

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"gtihub.com/KayoRonald/crud-mux-http/database"
 	"gtihub.com/KayoRonald/crud-mux-http/handle"
 	"gtihub.com/KayoRonald/crud-mux-http/middleware"
 	"gtihub.com/KayoRonald/crud-mux-http/middleware/logger"
+	"gtihub.com/KayoRonald/crud-mux-http/router"
 )
 
 func main() {
@@ -16,48 +17,14 @@ func main() {
 	r.Use(logger.New)
 	r.Use(middleware.CorsMiddleware)
 	r.NotFoundHandler = http.HandlerFunc(handle.NotFound)
-	r.HandleFunc("/", GetTasks).Methods("GET")
-	r.HandleFunc("/{id}", GetTasksByID).Methods("GET")
-	r.HandleFunc("/", PostTasks).Methods("POST")
-	r.HandleFunc("/{id}", PutTasks).Methods("PUT")
-	r.HandleFunc("/{id}", DeleteTasks).Methods("DELETE")
-	log.Print("Rodando na porta 8080")
+	// connect database
+	database.ConnectDB()
+	r.HandleFunc("/", router.GetTasks).Methods("GET")
+	r.HandleFunc("/{id}", router.GetTasksByID).Methods("GET")
+	r.HandleFunc("/", router.PostTasks).Methods("POST")
+	r.HandleFunc("/{id}", router.PutTasks).Methods("PUT")
+	r.HandleFunc("/{id}", router.DeleteTasks).Methods("DELETE")
+	
+  log.Print("Rodando na porta 8080")
 	http.ListenAndServe(":8080", r)
-}
-
-func GetTasks(w http.ResponseWriter, rq *http.Request) {
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{
-		"message": "Hello method GET",
-	})
-}
-
-func GetTasksByID(w http.ResponseWriter, rq *http.Request) {
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{
-		"message": "Hello method GET",
-	})
-}
-
-func PostTasks(w http.ResponseWriter, rq *http.Request) {
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(map[string]string{
-		"message": "Hello method Post",
-	})
-}
-
-func PutTasks(w http.ResponseWriter, rq *http.Request) {
-	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{
-		"message": "Hello method Put",
-	})
-}
-func DeleteTasks(w http.ResponseWriter, rq *http.Request) {
-	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{
-		"message": "Hello method Delete",
-	})
 }
