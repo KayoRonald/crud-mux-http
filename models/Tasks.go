c2db223991f6bb505249fa2a4ad5ad89903720db
Kayo Ronald <kayoronald4321@gmail.com>
2023-07-17 17:03:24 +0000

    chore: remove json in struct model

--- models/Tasks.go
+++ models/Tasks.go
@@ -8,10 +8,10 @@ import (
 
 type Tasks struct {
 	gorm.Model
-	ID           string `json:"id" gorm:"type:uuid;primaryKey"`
-	Name         string `json:"name" gorm:"not null"`
-	Descripition string `json:"descripition" gorm:"not null"`
-	Done         bool   `json:"done"`
+	ID           string `gorm:"type:uuid;primaryKey"`
+	Name         string `gorm:"not null"`
+	Descripition string `gorm:"not null"`
+	Done         bool   
 	CreateAt     time.Time
 	UpdateAt     time.Time
 	DeleteAt     gorm.DeletedAt `gorm:"index"`
