57bf31003e4189c56904f86da9df3ae4827d3dca
Kayo Ronald <kayoronald4321@gmail.com>
2023-07-17 14:40:29 +0000

    feat: update log and adding json in model struct

--- middleware/logger/loggingMiddleware.go
+++ middleware/logger/loggingMiddleware.go
@@ -7,7 +7,7 @@ import (
 
 func New(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Sprintf("| %v | %v | %v |", r.Method, r.RequestURI, r.Host)
+		fmt.Printf("| %v | %v | %v |", r.Method, r.RequestURI, r.Host)
 		next.ServeHTTP(w, r)
 	})
 }
--- models/Tasks.go
+++ models/Tasks.go
@@ -8,10 +8,10 @@ import (
 
 type Tasks struct {
 	gorm.Model
-	ID           string `gorm:"type:uuid;primaryKey"`
-	Name         string `gorm:"not null"`
-	Descripition string `gorm:"not null"`
-	Done         bool
+	ID           string `json:"id" gorm:"type:uuid;primaryKey"`
+	Name         string `json:"name" gorm:"not null"`
+	Descripition string `json:"descripition" gorm:"not null"`
+	Done         bool   `json:"done"`
 	CreateAt     time.Time
 	UpdateAt     time.Time
 	DeleteAt     gorm.DeletedAt `gorm:"index"`
