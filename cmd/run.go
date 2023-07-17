ce363d01914cacec2f4a5e7dac5455ae9a88a7fb
Kayo Ronald <kayoronald4321@gmail.com>
2023-07-17 17:01:49 +0000

    feat: create router tasks done

--- cmd/run.go
+++ cmd/run.go
@@ -9,14 +9,15 @@ import (
 )
 
 func Run(r *mux.Router) error {
-  v1 := r.PathPrefix("/tasks/").Subrouter()
-  v1.HandleFunc("/", router.GetTasks).Methods("GET")
+	v1 := r.PathPrefix("/tasks/").Subrouter()
+	v1.HandleFunc("/", router.GetTasks).Methods("GET")
+	v1.HandleFunc("/done/", router.GetTasksDone).Methods("GET")
 	v1.HandleFunc("/{id}", router.GetTasksByID).Methods("GET")
 	v1.HandleFunc("/", router.PostTasks).Methods("POST")
 	v1.HandleFunc("/{id}", router.PutTasks).Methods("PUT")
 	v1.HandleFunc("/{id}", router.DeleteTasks).Methods("DELETE")
 
-  log.Print("Rodando na porta 8080")
-	http.ListenAndServe(":8080", r)
-  return nil
+	log.Print("Rodando na porta 3000")
+	http.ListenAndServe(":3000", r)
+	return nil
 }
