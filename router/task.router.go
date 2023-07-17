5dc8f31e42243ccfd00571b3be524c052ad4c770
Kayo Ronald <kayoronald4321@gmail.com>
2023-07-17 14:39:46 +0000

    feat: rename task and create parser body res user

--- router/task.router.go
+++ router/task.router.go
@@ -3,7 +3,15 @@ package router
 import (
 	"encoding/json"
 	"net/http"
+
+	// "gtihub.com/KayoRonald/crud-mux-http/utils"
 )
+
+type Teste struct {
+	Name         string `json:"name"`
+	Descripition string `json:"descripition"`
+}
+
 
 func GetTasks(w http.ResponseWriter, rq *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
@@ -23,10 +31,17 @@ func GetTasksByID(w http.ResponseWriter, rq *http.Request) {
 
 func PostTasks(w http.ResponseWriter, rq *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(map[string]string{
-		"message": "Hello method Post",
-	})
+  var teste Teste
+
+  err := json.NewDecoder(rq.Body).Decode(&teste)
+  if err != nil {
+    w.WriteHeader(http.StatusBadRequest)
+    json.NewEncoder(w).Encode(map[string]string{
+      "message": err.Error(),
+    })
+  }
+  w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(teste)
 }
 
 func PutTasks(w http.ResponseWriter, rq *http.Request) {
